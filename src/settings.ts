f6286111d492514bd5a1139660469c93abcda406
Alexandre Guibert <alexandre@evaneos.com>
2025-02-27 18:10:59 +0100

    fix config.schema

--- src/settings.ts
+++ src/settings.ts
@@ -7,3 +7,4 @@ export const PLATFORM_NAME = 'HomebridgeZzZenggeBleStrips';
  * This must match the name of your plugin as defined the package.json
  */
 export const PLUGIN_NAME = 'homebridge-zz-zengge-ble-strips';
+
